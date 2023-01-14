e09ca84dd17baa34496ae90e0cd663a867a2a30b
Alperen Albayrak <alperen.albayrak867@gmail.com>
2023-01-14 14:05:54 +0300

    fixed page icon

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* Add the favicon */}
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      {/* Add the favicon */}
+      {/* Note that the path doesn't include "public" */}
+
+      <Component {...pageProps} />
+    </>
+  );
+}
